b85c3bf562625e90db382cfb345916accd5d9630
DESKTOP-NMVP5JG\user01 <shokupan21@gmail.com>
2021-02-21 18:00:15 +0900

    単体ジャンプ

---
+++ Assets/Main/Common/Script/Jump.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Jump : MonoBehaviour
+{
+
+    public int tapJumpButton(Rigidbody2D rb, int jumpCount, float jumpAddSpeed)
+    {
+        // 取り直す
+        float velocityX = rb.velocity.x;
+        // ジャンプ処理
+        if (Input.GetKey(KeyCode.X))
+        {
+            if (jumpCount == 2)
+            {
+                rb.velocity = new Vector2(velocityX, jumpAddSpeed);
+                return -1;
+            }
+        }
+
+        return jumpCount;
+
+    }
+}
--- Assets/Main/Common/Script/MovePrayer.cs
+++ Assets/Main/Common/Script/MovePrayer.cs
@@ -4,34 +4,80 @@ using UnityEngine;
 
 public class MovePrayer : MonoBehaviour
 {
-    public float speed;
+    // 最高速度
+    public float maxSpeed;
+    // 加速度
+    public float addSpeed;
+    // ジャンプ時の加算速度
+    public float jumpAddSpeed;
+    // ジャンプ可能かどうかの判定
+    public int jumpCount = -1;
 
+    Jump jumpClass = new Jump();
+
+
+    // 操作キャラか判定
+    public bool Player;
+
+    // Rigidbody2D格納
     private Rigidbody2D rb;
 
-    // Start is called before the first frame update
+    // 初期化
     void Start()
     {
+        // オブジェクトのrigidbodyの取得
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // 物理演算をしたい場合はFixedUpdateを使うのが一般的
-    void FixedUpdate()
+    // 更新時に実行
+    void Update()
     {
-        float horizontalKey = Input.GetAxis("Horizontal");
+        if (rb.velocity.y == 0 && jumpCount < 2)
+        {
+            jumpCount += 1;
+        }
 
-        //右入力で左向きに動く
-        if (horizontalKey > 0)
+
+        // 現在のX軸速度
+        float velocityX = rb.velocity.x;
+
+        //右入力で右向きに動く
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-            rb.velocity = new Vector2(speed, rb.velocity.y);
-        } //左入力で左向きに動く
-        else if (horizontalKey < 0)
+            // 速度が最高速度ではない場合
+            if (velocityX < maxSpeed)
+            {
+                // 初速はつける
+                if (velocityX == 0)
+                {
+                    rb.velocity = new Vector2(1.0f, rb.velocity.y);
+                }
+                rb.velocity = new Vector2(rb.velocity.x + addSpeed, rb.velocity.y);
+            }
+        }
+        //左入力で左向きに動く
+        else if (Input.GetKey(KeyCode.LeftArrow))
         {
-            rb.velocity = new Vector2(-speed, rb.velocity.y);
+            // 速度が最高速度ではない場合
+            if (velocityX > -maxSpeed)
+            {
+                // 初速はつける
+                if (velocityX == 0)
+                {
+                    rb.velocity = new Vector2(-1.0f, rb.velocity.y);
+                }
+                rb.velocity = new Vector2(rb.velocity.x - addSpeed, rb.velocity.y);
+            }
         }
         else
-        //ボタンを話すと止まる
+        //ボタンを離すと止まる
         {
             rb.velocity = new Vector2(0, rb.velocity.y);
         }
+
+        jumpCount = jumpClass.tapJumpButton(rb, jumpCount, jumpAddSpeed);
+
     }
 }
+
+
